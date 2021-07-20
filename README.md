93ff376f1c8a7591781d599aae9402a7e50a5d03
Hongjincoco <80315891+Hongjincoco@users.noreply.github.com>
2021-07-20 10:57:27 +0900

    perfect

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vodka
+Django - 웹페이지
+
+mysql connector python
+django
---
+++ tourists/config/config.py
@@ -0,0 +1,7 @@
+#MySQL접속 정보를 딕셔너리 형태로 저장한다.
+db_config={ 'host' : 'database-2.cwyjp1nzvdgo.ap-northeast-2.rds.amazonaws.com',
+            'database' : 'tourist_cctv',
+            'user' : 'tourist_destination',
+            'password' : 'tourist1234'}
+
+
